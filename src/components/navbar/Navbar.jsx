6d63ba99b32a3fb0ab02902b30ab95cf7b2546fb
meerimbolotova <meerimbolotova04gmail.com>
2023-06-23 15:19:32 +0600

    navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import "./Navbar.css";
 
 const Navbar = () => {
-  return <div></div>;
+  return (
+    <>
+      <div className="navbar">
+        <div className="main-navbar">
+          <div className="logo">
+            <img
+              src="https://my.p24.app/files/52a3e2cd-dac5-4d00-b296-3c84d33a4377.svg"
+              alt="logo"
+            />
+          </div>
+          <ul className="navbar-menu">
+            <li>Сеансы</li>
+            <li>Афиша</li>
+            <li>О нас</li>
+            <li>0500 000 005</li>
+          </ul>
+        </div>
+        <div className="navbar-bord"></div>
+      </div>
+    </>
+  );
 };
 
 export default Navbar;
