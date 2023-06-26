5eaa34ebd44b9a9354d75010098a2f334458254a
Magira888 <magiraaskarbekova@gmail.com>
2023-06-26 11:01:54 +0600

    pulling

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,7 +2,20 @@ import React from "react";
 import "./Home.css";
 
 const Home = () => {
-  return <div className="box"></div>;
+  return (
+    <div className="home">
+      <header className="main-header">
+        <div className="layers">
+          <div className="layer layers__base"></div>
+          <div className="layer layers__midle"></div>
+          <div className="layer layers__front"></div>
+        </div>
+      </header>
+      <article></article>
+    </div>
+  );
 };
+
+
 
 export default Home;
