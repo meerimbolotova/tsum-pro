f5c72189a069ddc72f85192b24c0d4953fec5c57
aidarbek312 <aidarbek312@gmail.com>
2023-06-27 14:17:32 +0600

    authhhh

---
+++ src/auth/authSlice.js
@@ -0,0 +1,23 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  user: "",
+  error: "",
+};
+
+export const authSlice = createSlice({
+  name: "@auth",
+  initialState,
+  reducers: {
+    setUser(state, action) {
+      state.user = action.payload;
+    },
+    setError(state, action) {
+      state.error = action.payload;
+    },
+  },
+});
+
+export const authReducer = authSlice.reducer;
+
+export const { setUser, setError } = authSlice.actions;
---
+++ src/components/auth/Register.jsx
@@ -0,0 +1,57 @@
+import * as React from "react";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { register } from "../../auth/authAction";
+
+export default function Register() {
+  const [email, setEmail] = React.useState("");
+  const [username, setUsername] = React.useState("");
+  const [password, setPassword] = React.useState("");
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleSubmit = () => {
+    if (!email.trim() || !username.trim() || !password.trim()) {
+      alert("Заполните поля!");
+      return;
+    }
+    let formData = new FormData();
+    formData.append("email", email);
+    formData.append("username", username);
+    formData.append("password", password);
+    dispatch(register({ formData, navigate }));
+  };
+
+  return (
+    <div
+      style={{ display: "flex", flexDirection: "column", alignItems: "center" }}
+    >
+      <h2>Войти</h2>
+      <input
+        placeholder="email"
+        id="email"
+        autoComplete="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        placeholder="username"
+        type="text"
+        id="username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <input
+        placeholder="password"
+        type="password"
+        id="password"
+        autoComplete="current-password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={handleSubmit}>регистрация</button>
+      <button onClick={() => navigate("/login")}>войти</button>
+    </div>
+  );
+}
