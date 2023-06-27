23d3f44187f4fbb72357c63eb9f87e8d9764e284
Magira888 <magiraaskarbekova@gmail.com>
2023-06-27 12:33:20 +0600

    save

--- src/components/Home/Card.jsx
+++ src/components/Home/Card.jsx
@@ -1,7 +1,22 @@
-import React from 'react';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getCards } from '../../stores/cards/cardAction';
 
 const Card = () => {
-  return <div></div>;
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getCards());
+  }, []);
+  const { cards } = useSelector(state => state.cards);
+  return (
+    <div className='card__container'>
+      {cards.map((elem, index) => (
+        <div style={{ width: '30%', height: '60%' }} key={index}>
+          <img src={elem.image} alt='error' />
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Card;
